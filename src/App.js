9ef1904b65e9f299c6119c0dfa0ec52cb94c68bd
Patricio Fajardo <apatricio.fajardoc@gmail.com>
2021-10-07 18:22:45 -0500

    Desarrollo de todos lo componentes necesarios para que la aplicacion funcion

--- src/App.js
+++ src/App.js
@@ -1,8 +1,73 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React, { Fragment, useState, useEffect } from "react";
+import Clima from "./components/Clima";
+import Error from "./components/Error";
+import Formulario from "./components/Formulario";
+import Header from "./components/Header";
 
 function App() {
-  return <h1>Obtener Clima</h1>;
+  // state de appjs
+  const [busqueda, guardarBusqueda] = useState({
+    ciudad: "",
+    pais: "",
+  });
+
+  const [consulta, guardarConsulta] = useState(false);
+  const [resultado, guardarResultado] = useState({});
+  const [error, guardarError] = useState(false);
+
+  const { ciudad, pais } = busqueda;
+
+  useEffect(() => {
+    const consultarAPI = async () => {
+      if (consulta) {
+        const appId = "e34c0d38fa742e22ab227c6e1a875f67";
+        const url = `http://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=${appId}`;
+
+        const respuesta = await fetch(url);
+        const resultado = await respuesta.json();
+        guardarResultado(resultado);
+        guardarConsulta(false);
+
+        // detecta si hubo resultados correcto en la consulta
+        if (resultado.cod === "404") {
+          guardarError(true);
+        } else {
+          guardarError(false);
+        }
+      }
+    };
+    consultarAPI();
+
+    // eslint-disable-next-line
+  }, [consulta]);
+
+  let componente;
+  if (error) {
+    componente = <Error mensaje="No hay resultados" />;
+  } else {
+    componente = <Clima resultado={resultado} />;
+  }
+  return (
+    <Fragment>
+      <Header titulo="Clima React"></Header>
+      <div className="contenedor-form">
+        <div className="container">
+          <div className="row">
+            <div className="col m12 s12">
+              <Formulario
+                busqueda={busqueda}
+                guardarBusqueda={guardarBusqueda}
+                guardarConsulta={guardarConsulta}
+              />
+            </div>
+          </div>
+          <div className="row">
+            <div className="col m12 s12">{componente}</div>
+          </div>
+        </div>
+      </div>
+    </Fragment>
+  );
 }
 
 export default App;
