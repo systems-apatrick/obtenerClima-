36100df98891c6253e8e46ffab411a027420c1d9
Patricio Fajardo <apatricio.fajardoc@gmail.com>
2021-10-08 18:14:59 -0500

    Agreacion de https a la api

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ function App() {
         console.log("Comienza a hacer la consulta");
         guardarCargando(true);
         const appId = process.env.REACT_APP_ID_WEATHER;
-        const url = `http://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=${appId}`;
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=${appId}`;
 
         const respuesta = await fetch(url);
         const resultado = await respuesta.json();
